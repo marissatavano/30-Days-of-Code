81d088ebe93a7bacba9799f4145e4e87972e6032
Marissa Tavano <tavanom1@tcnj.edu>
2017-12-08 15:11:39 -0500

    Create day05.cpp

---
+++ day05.cpp
@@ -0,0 +1,40 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int sum;
+    
+    for(int i = 1; i < 11; i++) {
+        sum = n * i;
+        cout << n << " x " << i << " = " << sum << endl;
+    }
+    
+    
+    return 0;
+}
